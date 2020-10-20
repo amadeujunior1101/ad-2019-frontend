426bb82f9331ed625028337c60ec89590cfb4d63
Amadeu Júnior <amadeujunior00@gmail.com>
2020-10-20 13:08:07 -0300

    Alteração service/api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const api = axios.create({
-  baseURL: BASE_URL || 'http://192.168.1.8:3333',
+  baseURL: process.env.REACT_APP_BASE_URL || 'http://192.168.1.8:3333',
 });
 
 export default api;
